3e2733d04f108f22edc19889248b0b09f095a061
Deniz🌊 <kdendag@gmail.com>
2025-01-17 17:57:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ facts_list = [
 # Ana sayfa: Ziyaretçilere bir hoş geldiniz mesajı gösteriliyor
 @app.route("/")
 def index():
-    return f'<h1>MERHABA! Bu sayfada, teknolojik bağımlılıklar hakkında birkaç ilginç gerçeği öğrenebilirsiniz!</h1>'
+    return f'<h1>Teknolojik bağımlılıklar hakkında birkaç ilginç gerçeği öğrenebilirsiniz!</h1><a href="/rastgele_gercek">İlginç Gerçekler</a>'
 
 # Rastgele bir gerçeği kullanıcıya gösteren sayfa
 @app.route("/rastgele_gercek")
